44b3693de60812c1a5bc8621a27d0e2ecd65b49c
JJ15673 <juliajerenkiewicz@gmail.com>
2024-05-21 19:32:23 +0200

    main.py
    Switch to password generator

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-meme_dict = {
-    "CRINGE": "Coś wyjątkowo dziwnego lub zawstydzającego",
-    "LOL": "Częsta reakcja na coś zabawnego",
-    "OMG": "Wyrażenie zdziwienia lub zdumienia",
-    "WTF": "Wyrażenie zdziwienia, zdumienia lub oburzenia",
-    "BRB": "Be Right Back, chwila przerwy",
-    "IDK": "I Don't Know, nie wiem",
-    "AFK": "Away From Keyboard, nieobecny przy klawiaturze",
-    
-}
-  
-while True:
-    word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
-
-    if word in meme_dict.keys():
-        print(meme_dict[word])
-    else:
-        print("To słowo nie zostało odnalezione w naszej bibliotece :(")
+import random
+znaki = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+dl = int(input("długość hasła:"))
+password = ""
+for i in range(dl):
+    password += znaki[random.randint(0,len(znaki))] 
+print(password)
